bb4ad78adb586323e53ff3f138b55fbb4dfe0662
mjpagan <mike.pagan@netcenter.net>
2017-03-04 22:14:56 -0600

    First working version
    Creates users from CSV file in a specified OU

---
+++ Create-BulkADUsers-CSV.ps1
@@ -0,0 +1,37 @@
+﻿Try
+{
+  Import-Module ActiveDirectory -ErrorAction Stop
+}
+Catch
+{
+  Write-Host "[ERROR]`t ActiveDirectory Module couldn't be loaded. Script will stop!"
+  Exit 1
+}
+
+#----------------------------------------------------------
+#STATIC VARIABLES
+#----------------------------------------------------------
+$path     = Split-Path -parent $MyInvocation.MyCommand.Definition
+$newpath  = "C:\Scripts\NewUsers.csv"
+$log      = "C:\Scripts\Create-BulkADUsers-CSV.log"
+$date     = Get-Date
+$addn     = (Get-ADDomain).DistinguishedName
+$dnsroot  = (Get-ADDomain).DNSRoot
+$i        = 1
+
+"Processing started (on " + $date + "): " | Out-File $log -append
+"--------------------------------------------" | Out-File $log -append
+
+Import-Csv $newpath | ForEach-Object {
+ $userPrincinpal = $_.samAccountName + "@$dnsroot"
+ $ou = $_.ParentOU + ",$addn"
+New-ADUser -Name $_.Name `
+ -Path $ou `
+ -SamAccountName  $_.samAccountName `
+ -UserPrincipalName  $userPrincinpal `
+ -AccountPassword (ConvertTo-SecureString "Password123" -AsPlainText -Force) `
+ -ChangePasswordAtLogon $false  `
+ -Enabled $true
+ Write-Host "[INFO]`t Created new user : $($userPrincinpal)" 
+ }
+#Add-ADGroupMember "Domain Admins" $_.samAccountName}
